a8a379637218cfe8f99e66179c50e47ec0f21432
Fabio Lettieri <fabiolettieri09@gmail.com>
2022-12-05 20:07:16 -0300

    Ajustes de retornos 404

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,8 +27,9 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
-		return cozinhaRepository.buscar(id);
+	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
+		Cozinha cozinha = cozinhaRepository.buscar(id);
+		return (cozinha == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(cozinha));
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
